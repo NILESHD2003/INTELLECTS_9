fcddbbb4b00d0c4043131e4d38eb0b5e7c3cf7ee
Nilesh Deshpande <nileshdeshpandework@gmail.com>
2024-03-17 16:33:12 +0530

    changed mail func

--- server/mail/emailVerification.js
+++ server/mail/emailVerification.js
@@ -66,15 +66,13 @@ const otpTemplate = (otp) => {
 	
 	<body>
 		<div class="container">
-			<a href="/"><img class="logo"
-					src="https://res.cloudinary.com/dflgxjjm7/image/upload/v1708256301/StartUpSphere/h78yqvxnhkbzxpfey0pf.png" alt="StudyNotion Logo"></a>
 			<div class="message">OTP Verification Email</div>
 			<div class="body">
 				<p>Dear User,</p>
-				<p>Thank you for registering with StartUpSphere. To complete your registration, please use the following OTP
+				<p>Thank you for registering with Job Board. To complete your registration, please use the following OTP
 					(One-Time Password) to verify your account:</p>
 				<h2 class="highlight">${otp}</h2>
-				<p>This OTP is valid for 5 minutes. If you did not request this verification, please disregard this email.
+				<p>This OTP is valid for 3 minutes. If you did not request this verification, please disregard this email.
 				Once your account is verified, you will have access to our platform and its features.</p>
 			</div>
 			<div class="support">If you have any questions or need assistance, please feel free to reach out to us at <a
