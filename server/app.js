33048802a69ac7b992cb38938f2381d930dbe1b9
Nilesh Deshpande <nileshdeshpandework@gmail.com>
2024-03-17 08:55:55 +0530

    fixed cors bug

--- server/app.js
+++ server/app.js
@@ -7,35 +7,14 @@ const { cloudinaryConnect } = require("./Config/cloudinary");
 const fileUpload = require("express-fileupload");
 const dotenv = require("dotenv");
 
-app.use(express.json());
-
-// importing routes
-const userRoutes = require("./Routes/user");
-const jobRoutes = require("./Routes/jobs");
-// const applicationRoutes = require("./Routes/")
-
-// defining routes
-app.use("/api/v1/auth", userRoutes);
-app.use("/api/v1/jobs", jobRoutes);
-// app.use("/api/v1/application", )
-
 dotenv.config();
 database.connect();
 
 // Middlewares
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors()); // Enable CORS for all routes
 app.use(
-  cors({
-    origin: "*", // Allow requests from any origin
-    credentials: true,
-    methods: ['GET', 'POST'],
-  })
-  );
-  // CORS preflight handling for all routes
-  app.options("*", cors()); // Respond to preflight requests with CORS headers
-
-  app.use(
   fileUpload({
     useTempFiles: true,
     tempFileDir: "/tmp/",
@@ -43,6 +22,14 @@ app.use(
 );
 
 cloudinaryConnect();
+
+// importing routes
+const userRoutes = require("./Routes/user");
+const jobRoutes = require("./Routes/jobs");
+
+// defining routes
+app.use("/api/v1/auth", userRoutes);
+app.use("/api/v1/jobs", jobRoutes);
 
 const PORT = process.env.PORT || 8000;
 
